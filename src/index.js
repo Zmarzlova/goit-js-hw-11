d749c32bbdb331566e29697d8a09aa32526473de
Дана Змарзлова <99808397+Zmarzlova@users.noreply.github.com>
2023-03-27 21:41:07 +0300

    add fixed hw

--- src/index.js
+++ src/index.js
@@ -9,34 +9,34 @@ const refs = {
   input: document.querySelector('input'),
   gallery: document.querySelector('.gallery'),
   btnLoadMore: document.querySelector('.load-more'),
-};
+}
 
-let page = 1; 
+let page = 1 
 
 refs.btnLoadMore.style.display = 'none'; // ховаємо кнопку
-refs.form.addEventListener('submit', onSearch); 
-refs.btnLoadMore.addEventListener('click', onBtnLoadMore); 
+refs.form.addEventListener('submit', onSearch) 
+refs.btnLoadMore.addEventListener('click', onBtnLoadMore) 
 
 
-function onSearch(evt) {
-  evt.preventDefault(); 
+function onSearch(e) {
+  e.preventDefault() 
 
-  page = 1;
-  refs.gallery.innerHTML = ''; // очищення попереднього вмісту галереї
+  page = 1
+  refs.gallery.innerHTML = '' 
 
   const name = refs.input.value.trim(); // обрізання пробілів до і після слова
 
 
   if (name !== '') {
-    pixabay(name);
+    fetchGallery(name);
 
   } else {
-    refs.btnLoadMore.style.display = 'none';
+    refs.btnLoadMore.style.display = 'none'
 
     // повідомлення про те, що НЕ знайдено жодного зображення
     return Notiflix.Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
-    );
+    )
   }
 }
 
@@ -44,16 +44,16 @@ function onSearch(evt) {
 function onBtnLoadMore() {
   const name = refs.input.value.trim();
   page += 1; // додаємо +1 сторінку яка має +40 картинок
-  pixabay(name, page); // 
+  fetchGallery(name, page); // 
 }
 
 // отримання зображень
-async function pixabay(name, page) {
+async function fetchGallery(name, page) {
   const API_URL = 'https://pixabay.com/api/';
 
-  // запит на бекенд
-  const options = {
-    params: {
+  // список параметрів запит на бекенд
+    const options = {
+      params: {
       key: '34757422-0fa0b656f1bfd5f5e4a4b9d75', // мій персональний ключ з pixabay
       q: name,
       image_type: 'photo',
@@ -65,12 +65,10 @@ async function pixabay(name, page) {
   };
 
   try {
-    // отримання відповіді-результату від бекенду
     const response = await axios.get(API_URL, options);
 
-   
     notification(
-      response.data.hits.length, // довжина всіх знайдених зображень
+      response.data.hits.length, 
       response.data.total 
     );
 
@@ -81,35 +79,33 @@ async function pixabay(name, page) {
 }
 
 
-function createMarkup(arr) {
-  const markup = arr.hits
-    .map(
-      item =>
-        `<a class="photo-link" href="${item.largeImageURL}">
-            <div class="photo-card">
-            <div class="photo">
-            <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy"/>
-            </div>
-                    <div class="info">
-                        <p class="info-item">
-                            <b>Likes</b>
-                            ${item.likes}
-                        </p>
-                        <p class="info-item">
-                            <b>Views</b>
-                            ${item.views}
-                        </p>
-                        <p class="info-item">
-                            <b>Comments</b>
-                            ${item.comments}
-                        </p>
-                        <p class="info-item">
-                            <b>Downloads</b>
-                            ${item.downloads}
-                        </p>
-                    </div>
-            </div>
-        </a>`
+function createMarkup(elements) {
+  const markup = elements.hits
+    .map(item => {
+       return `<div class="photo-card">
+        <a  href="${item.largeImageURL}">
+              <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy"/>
+          </a>
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b>
+            ${item.likes}
+          </p>
+          <p class="info-item">
+            <b>Views</b>
+            ${item.views}
+          </p>
+          <p class="info-item">
+             <b>Comments</b>
+            ${item.comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads</b>
+            ${item.downloads}
+          </p>
+         </div>
+       </div>`
+      }
     )
     .join(''); 
   refs.gallery.insertAdjacentHTML('beforeend', markup); 
@@ -118,32 +114,34 @@ function createMarkup(arr) {
 }
 
 // екземпляр модального вікна слайдера-зображень
-const simpleLightBox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt', // опис
+const simpleLightBox = new SimpleLightbox('.photo-card a', {
+  captionsData: 'alt', 
   captionDelay: 250, 
 });
 
 function notification(length, totalHits) {
+  // refs.btnLoadMore.classList.add('is-hidden')
   if (length === 0) {
 
       // повідомлення про те, що НЕ знайдено жодного зображення
     Notiflix.Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
+    // refs.btnLoadMore.classList.add('is-hidden')
     return;
   }
 
   if (page === 1) {
-    refs.btnLoadMore.style.display = 'flex'; // показуємо кнопк
+    refs.btnLoadMore.style.display = 'flex'; 
 
     // повідомлення про кількість знайдених зобрежнь
-    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`)
   }
 
   if (length < 40) {
     refs.btnLoadMore.style.display = 'none'; // ховаємо кнопку
 
-      // повідомлення про те, що більше вже немає зображень
+      
     Notiflix.Notify.info(
       "We're sorry, but you've reached the end of search results."
     );
